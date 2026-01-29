1c412413d1772ad572a166118f0ef30eba7d0ba3
YuviDeepak <yuvideepak190@gmail.com>
2026-01-29 13:20:53 +0530

    Task Compeleted

---
+++ dom/js/script.js
@@ -0,0 +1,28 @@
+// let bgchange= ()=>{
+//     let clr1=document.getElementById("bg1")
+//     let clr2=document.getElementById("bg2")
+//     let bbbg=document.getElementById("bdy")
+//     bbbg.style.backgroundImage = `linear-gradient(to right ,${clr1.value}, ${clr2.value})`;
+// }
+
+let count = 0
+let add_to_cart = document.getElementById("Addtocart")
+
+let incre = ()=>{
+    if(count<10){
+        count++
+        add_to_cart.innerText=count
+    }
+}
+
+let decre = ()=>{
+    if(count>0){
+        count--
+        add_to_cart.innerText=count
+    }
+}
+
+let re = ()=>{
+    count=0
+    add_to_cart.innerText=count
+}
